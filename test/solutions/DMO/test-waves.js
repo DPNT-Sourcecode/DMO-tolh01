81b9596a256c3740d77fc9ecb3cc215231aef6c6
sbx_user1051 <sbx_user1051@169.254.45.167>
2025-10-23 18:26:39 +0000

    Thu Oct 23 18:26:39 UTC 2025

--- test/solutions/DMO/test-waves.js
+++ test/solutions/DMO/test-waves.js
@@ -2,21 +2,32 @@ var mocha = require('mocha')
 var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
-const DemoRound4n5Solution = require('../../../lib/solutions/DMO/demo_round4n5_solution');
+const waves = require('../../../lib/solutions/DMO/waves');
 
 describe('DMO challenge: Waves characterisation tests', function() {
     let solution;
 
     beforeEach(function() {
-        solution = new DemoRound4n5Solution();
+        solution = new waves.Main();
     });
+
+    const runWaves = (numWaves) => {
+        const outputSink = new waves.StringOutputSink();
+        const inputProvider = {
+            fetch: () => numWaves,
+            close: () => {}
+        }
+        solution.run(outputSink, inputProvider);
+        return outputSink.content();
+    };
 
     describe('Waves', function() {
         it('should output waves', async function() {
-            assert.equal(solution.waves(1), "____....~~~~''''~~~~....____");
-            assert.equal(solution.waves(3), "_.~'~.__.~'~.__.~'~._");
-            assert.equal(solution.waves(4), "_.~'~.__.~'~.__.~'~.__.~'~._");
-            assert.equal(solution.waves(5), "_.~'~.__.~'~.__.~'~.__.~'~.__.~'~._");
+            assert.equal(runWaves(1), "____....~~~~''''~~~~....____");
+            assert.equal(runWaves(3), "_.~'~.__.~'~.__.~'~._");
+            assert.equal(runWaves(4), "_.~'~.__.~'~.__.~'~.__.~'~._");
+            assert.equal(runWaves(5), "_.~'~.__.~'~.__.~'~.__.~'~.__.~'~._");
         });
     });
 });
+
